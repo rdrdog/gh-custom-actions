1a11f83f4706f1d0f373840db56bda303c19e12a
Robert Redgwell <robert.redgwell@clearpoint.co.nz>
2022-01-26 23:39:18 +1300

    Removes manifest_images keep from init step

--- actions/initialise/initialise.js
+++ actions/initialise/initialise.js
@@ -17,17 +17,17 @@ const runAsync = async () => {
     // Write the git state to file:
     await fs.promises.writeFile(manifestGitStateKey, JSON.stringify(gitState, null, 2));
     // Create the manifest images folder and a .keep file to make sure it's uploaded as an artifact
-    await fs.promises.mkdir(manifestImagesKey, { recursive: true });
-    const manifestImagesPlaceholderFile = path.join(manifestImagesKey, '.keep');
-    await fs.promises.writeFile(manifestImagesPlaceholderFile, '');
+    // await fs.promises.mkdir(manifestImagesKey, { recursive: true });
+    // const manifestImagesPlaceholderFile = path.join(manifestImagesKey, '.keep');
+    // await fs.promises.writeFile(manifestImagesPlaceholderFile, '');
 
     if (!(await uploader.uploadArtifactAsync(manifestGitStateKey, manifestGitStateKey))) {
       throw Error('Unable to upload git state artifact');
     }
 
-    if (!(await uploader.uploadArtifactAsync(manifestImagesKey, manifestImagesPlaceholderFile))) {
-      throw Error('Unable to upload manifestImages folder');
-    }
+    // if (!(await uploader.uploadArtifactAsync(manifestImagesKey, manifestImagesPlaceholderFile))) {
+    //   throw Error('Unable to upload manifestImages folder');
+    // }
   }
   catch (error) {
     core.setFailed(error.message);
