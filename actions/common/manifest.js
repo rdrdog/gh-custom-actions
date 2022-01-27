6d1f665f503a3ab17a408c62226d9fef1fbcbd28
Robert Redgwell <robert.redgwell@clearpoint.co.nz>
2022-01-28 11:38:19 +1300

    Implements manifest

--- actions/common/manifest.js
+++ actions/common/manifest.js
@@ -1,11 +1,33 @@
-module.exports = {
-    getImageNameAndTagAsync: async (imageName) => {
-    }
+const core = require("@actions/core");
+const fs = require("fs");
+const artifactHandler = require("./artifact-handler");
 
-    // this returns     
-    // Input:
-    // ‘api’
-    // output:
-    // ‘example/api:eb43c28-1643260380914’
+const manifestImagesKey = "manifest_images";
+
+// Gets the fully qualidifed image name and tag for the specified image name (key)
+// e.g. given: "api", result: "ecr.example.org/example/api:abc123de"
+const getImageNameAndTagAsync = async (imageName) => {
+
+  // We only need to download the manifest once:
+  if (!getImageNameAndTagAsync._hasDownloaded) {
+    await artifactHandler.downloadArtifactAsync(manifestImagesKey);
+    getImageNameAndTagAsync._hasDownloaded = true;
+  }
+
+  return await fs.promises.readFile(
+    imageName,
+    { encoding: "utf8" }
+  );
 }
 
+const storeImageNameAndTagAsync = async (imageName, fqImageName, imageTag) => {
+  await fs.promises.writeFile(imageName, `${fqImageName}:${imageTag}`);
+
+  await artifactHandler.uploadArtifactAsync(manifestImagesKey, imageName);
+}
+
+
+module.exports = {
+  getImageNameAndTagAsync,
+  storeImageNameAndTagAsync,
+};
